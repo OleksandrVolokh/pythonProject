8c050d70e220d1346e128c2f5d412e602848ae7b
oleksandrvolokh <qaoleksandrvolok@gmail.com>
2023-04-26 23:29:55 +0700

    Commit 1

--- lesson5.py
+++ lesson5.py
@@ -61,9 +61,42 @@
 # -------------------------- GIT -----------------------
 # File modification history control!!!!!!!!!!!
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
+#
+# def mult(a, b):
+#     return a*b
 
-def mult(a, b):
-    return a*b
+# --------------- homework -----------
+
+class Car:
+    t_class = 'Transport'
+    def __init__(self, name, make, model):
+        self.name = name
+        self.make = make
+        self.model = model
+
+
+    def run(self):
+        return 'I can run for 5000 miles with one oil fill!'
+
+
+    def get_name(self):
+        return f'Hello! My owner calls me {self.name}'
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+car1 = Car('Leopard', 'Honda', 'Grey')
+print(car1.get_name())
+print(car1.make)
+print(car1.t_class)
+
+car2 = Car('"Runner"', 'Toyota', 'Camry')
+print(car2.__dict__)
+print(car2.make)
+car2.name = '"Fast Runner"'
+print(car2.get_name())
+
+
 
